8519248078483d7af94e71261df043d623413439
Aexplorer1 <1093038072@qq.com>
2025-04-24 08:34:37 +0800

    修复模块依赖问题，更新构建配置

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module slideshow {
     requires javafx.controls;
     requires javafx.graphics;
     requires com.google.gson;
+    requires java.logging;
     
     exports slideshow;
     exports slideshow.model;
